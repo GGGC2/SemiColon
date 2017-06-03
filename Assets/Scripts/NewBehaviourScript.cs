0075674f7e02c52c1f0ad572ff9cc22c98842a3a
taeilyoon <zkrxpdlf0808@gmail.com>
2017-06-03 15:08:34 +0900

    씬이동, 페인드 인아웃,

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+	public bool fade=false;
+	public float gameTime=0f;
+	public float speed = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(fade){
+			gameTime = gameTime + Time.deltaTime;
+			GetComponent<Image> ().color = new Color (0, 0, 0, gameTime);
+		}
+	}
+}
