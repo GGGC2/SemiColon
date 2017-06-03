2c80e8b9aece8310a6169b38027f3c331f69134b
EQwon <bluetwintail@naver.com>
2017-06-03 12:53:08 +0900

    기본 구조 짬

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed;
+	public float jumpSpeed;
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(KeyCode.RightArrow)) //오른쪽으로 이동, y 방향 이동속도는 그대로 유지
+		{
+
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow)) //왼쪽으로 이동, y 방향 이동속도는 그대로 유지
+		{
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) //점프 횟수는 1번, 바닥에 착지하면 초기화, x방향 이동속도 그대로 유지
+		{
+
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Z)) //중력 반전 횟수 1번, 바닥에 착지하면 초기화
+		{
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.X)) //공간 반전 횟수 무제한, x나 y 방향 이동속도는 부호만 바뀌고 그대로 유지
+		{
+
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D coll) //바닥에 착지하는 것을 감지, 점프나 중력 반전 횟수 초기화 시켜야함
+	{
+
+	}
+}
