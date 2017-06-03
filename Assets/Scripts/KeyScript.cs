39a4e3dec9dd532ee5fccff0afc3dcc10834f357
Kid7ho <jinmyoung.hyun@gmail.com>
2017-06-03 15:04:30 +0900

    키 스크립트

---
+++ Assets/Scripts/KeyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyScript : MonoBehaviour {
+
+    public int keytimes = 0;
+
+    public void getkey()
+    {
+        keytimes--;
+    }
+
+    public void opendoor()
+    {
+        if(keytimes == 0)
+        {
+            //스테이지 넘기기
+        }
+    }
+}
