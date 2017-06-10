e71f4b99a84ba514dc35fbc1bce08d3a8dd750e8
sprinter89 <sprinter8975@naver.com>
2017-06-10 14:04:27 +0900

    뭔가 달라졌습니다

--- Assets/Scripts/ClearScript.cs
+++ Assets/Scripts/ClearScript.cs
@@ -19,7 +19,6 @@ public class ClearScript : MonoBehaviour {
 
 	private void OnTriggerEnter2D(Collider2D coll) //바닥에 착지하는 것을 감지, 점프나 중력 반전 횟수 초기화 시켜야함
 	{
-		Debug.Log ("whlkfds");
 		if (coll.gameObject.tag == "Key") {
 			Getkey = true;
 			Debug.Log ("True!!");
